47e8790b796f4ab37114df9df010a079b143d660
dota2 <dota2@poczta.onet.pl>
2020-03-22 16:20:00 +0100

    display decimal by regions

---
+++ culturies/CashDispenser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace culturies
+{
+    static class CashDispenser
+    {
+        private static decimal Money { get; set; }
+
+        static CashDispenser()
+        {
+            Money = 12345.1498m;
+        }
+
+        public static void DisplayAccountBalacne(IFormatProvider serviceProvider)
+        {
+            Console.WriteLine(Money.ToString("F", CultureInfo.CreateSpecificCulture(serviceProvider.ToString())));
+        }
+    }
+}
---
+++ culturies/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace culturies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //decimal salary = 9999.9199m;
+
+            //Console.WriteLine(salary.ToString("F", CultureInfo.CreateSpecificCulture("en-US")));
+
+            ////CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+
+            //Console.WriteLine(salary.ToString("F"));
+
+            //GetService(CultureInfo.CurrentCulture);
+
+            //Display(RoundNumber(salary));
+
+            CultureInfo cultureInfo = CultureInfo.CurrentCulture;
+
+            CashDispenser.DisplayAccountBalacne(cultureInfo);
+
+            Console.ReadKey();
+        }
+
+        static void GetService(IFormatProvider serviceProvider)
+        {
+            Console.WriteLine(serviceProvider.ToString());
+        }
+
+        static decimal RoundNumber(decimal number)
+        {
+            return Math.Round(number, 3);
+        }
+
+        static void Display (decimal number)
+        {
+            Console.WriteLine(number.ToString("F", CultureInfo.InvariantCulture));
+            Console.WriteLine(number);
+        }
+    }
+}
